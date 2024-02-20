c08e7d858c04c9826df63099b613896fe012d96d
rauner <29773634+rauner@users.noreply.github.com>
2024-02-20 10:04:39 +0100

    bugfix frontend

--- src/main.rs
+++ src/main.rs
@@ -3,12 +3,18 @@ use assistant::{assistant_chat_handler, create_assistant, create_ressources, DB}
 use axum::{extract::Extension, routing::get_service, routing::post, Router};
 use dotenv::dotenv;
 use sqlx::SqlitePool;
-use std::net::SocketAddr;
 use tower_http::services::ServeDir;
+
+use reqwest::StatusCode;
+
 // Define a function to create the Axum app with the database pool and assistant.
 async fn app(db_pool: SqlitePool, assistant_id: String) -> Router {
     Router::new()
         .route("/assistant", post(assistant_chat_handler)) // Updated route
+        .nest_service(
+            "/", // Serve static files at the root of the domain
+            get_service(ServeDir::new("static"))
+            )
         .layer(Extension(db_pool))
         .layer(Extension(assistant_id)) // Add the assistant ID as a layer
 }
@@ -43,27 +49,9 @@ async fn main() {
     };
     let db_pool = db.pool;
     // Define the app with routes and static file serving
-    let app = Router::new()
-        .route("/assistant", post(assistant_chat_handler))
-        .nest(
-            "/", // Serve static files at the root of the domain
-            get_service(ServeDir::new("static")).handle_error(|error| async move {
-                (
-                    StatusCode::INTERNAL_SERVER_ERROR,
-                    format!("Unhandled internal error: {}", error),
-                )
-            }),
-        )
-        .layer(Extension(db_pool))
-        .layer(Extension(assistant.id));
-    // Define the address to serve on
-    let addr = SocketAddr::from(([0, 0, 0, 0], 3000));
-    // Run the server
-    Server::bind(&addr)
-        .serve(app.into_make_service())
+    let server = tokio::net::TcpListener::bind(&"0.0.0.0:3000")
         .await
-        .expect("Failed to start server");
-
+        .expect("Failed to bind server to address");
     let router = app(db_pool, assistant.id).await; // Pass the assistant ID to the app
     axum::serve(server, router.into_make_service())
         .await
